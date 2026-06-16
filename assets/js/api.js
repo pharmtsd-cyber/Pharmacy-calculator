a46c1630be8ae498ddf86e20b3cee9818c656d3a
pharmachris <pharm.tsd@gmail.com>
2026-06-16 18:04:44 +0800

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -6,7 +6,7 @@
  * @returns {Promise<any>}
  */
 async function fetchFromGAS(action) {
-    if (!CONFIG.GAS_API_URL || CONFIG.GAS_API_URL === "您的GAS網頁應用程式網址") {
+    if (!CONFIG.GAS_API_URL || CONFIG.GAS_API_URL === "https://script.google.com/macros/s/AKfycbxEnA79SWNy5m-sVhVl7wxMINbI3wXLI5Bsz3MbNGSgowU1WE_Lr40pDLu712wTATzA/exec") {
         console.error("尚未設定 GAS API 網址");
         return null;
     }
