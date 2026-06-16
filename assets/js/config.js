48a1d6e180717509bf257454bb7a4c936dc7d3a2
pharmachris <pharm.tsd@gmail.com>
2026-06-16 17:41:34 +0800

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,14 @@
+// assets/js/config.js
+
+// 部署核心提示：請替換為您在第一階段部署的 Google Apps Script Web App 網址
+const CONFIG = {
+    GAS_API_URL: "https://script.google.com/macros/s/AKfycbxEnA79SWNy5m-sVhVl7wxMINbI3wXLI5Bsz3MbNGSgowU1WE_Lr40pDLu712wTATzA/exec",
+    VERSION: "v1.0.0"
+};
+
+// 全域暫存資料庫
+const STORE = {
+    drugs: [],
+    parameters: [],
+    formulas: []
+};
